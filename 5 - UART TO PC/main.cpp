94bff45c7eb6f3675ee643be27e080e82a753118
Thushanth <thushanth.sithambararajan@mail.concordia.ca>
2023-06-28 19:55:31 -0400

    Update to Serial String
    Needs work.. Must be able to remove warning and change datatype handling.
    Design to replace Serial.Println() from arduino.

--- 5 - UART TO PC/main.cpp
+++ 5 - UART TO PC/main.cpp
@@ -10,12 +10,22 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 void transmit(char a)
 {
 	UDR0 = a;
 	while((UCSR0A & 0x40) == 0);	//wait for usart transmit flag to reset
 	_delay_ms(100);
+}
+
+void serialPrint(char *a)
+{
+	while((*a) != '\0')
+	{
+		transmit(*a);
+		a++;
+	}
 }
 
 int main(void)
@@ -27,11 +37,12 @@ int main(void)
 	UCSR0C = 0x06; //UMSEL1 UMSEL0 for async USART. USBSn 1 stop bit. UCSZ2 from B and UCSZ1-0 8-bit TX on Rising, RX on Falling
 	UBRR0H = 0x00; 
 	UBRR0L = 0x67; //from table 20-7 9600 bps 103 to hex is 67. on 16MHZ Crystal
-	transmit('H');
+/*	transmit('H');
 	transmit('E');
 	transmit('L');
 	transmit('L');
-	transmit('O');
+	transmit('O');*/
+	serialPrint("Nice to Meet you \n \r \0");
     while (1) 
     {
     }
